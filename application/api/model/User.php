9617ead2fd6acd6c9d39b176fd9f0a86a377b113
feizai1985 <570535031@qq.com>
2018-03-13 17:26:18 +0800

    2018/3/6

--- application/api/model/User.php
+++ application/api/model/User.php
@@ -12,7 +12,7 @@ namespace app\api\model;
 class User extends BaseModel
 {
    public static function getByOpenId($openid){
-       $user=self::where('openid','=',$openid);
+       $user=self::where('openid','=',$openid)->find();
        return $user;
    }
 
